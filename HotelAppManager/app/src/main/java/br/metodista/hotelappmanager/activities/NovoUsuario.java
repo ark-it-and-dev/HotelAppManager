c96093783b3aadb5314293fe23b08b49137bdb6c
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-10-26 00:08:26 -0200

    + Criacao default da Empty activity NovoUsuario e alteracao altomatica no Manifest

---
+++ HotelAppManager/app/src/main/java/br/metodista/hotelappmanager/activities/NovoUsuario.java
@@ -0,0 +1,15 @@
+package br.metodista.hotelappmanager.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.metodista.hotelappmanager.R;
+
+public class NovoUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_usuario);
+    }
+}
