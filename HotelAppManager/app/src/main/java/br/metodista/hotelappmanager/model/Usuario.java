69e65ccbf86293568c2d1a26db1e6e7fb703e541
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-10-26 00:33:08 -0200

    * Conclusao da classe Usuario

--- HotelAppManager/app/src/main/java/br/metodista/hotelappmanager/model/Usuario.java
+++ HotelAppManager/app/src/main/java/br/metodista/hotelappmanager/model/Usuario.java
@@ -4,4 +4,43 @@ package br.metodista.hotelappmanager.model;
  * Created by Gustavo Assalin on 26/10/2015.
  */
 public class Usuario {
+    private static Usuario usuario;
+
+    private String login;
+    private String senha;
+
+    private Usuario() {}
+
+    public static Usuario getInstance() {
+        if(usuario == null) {
+            return new Usuario();
+        } else {
+            return usuario;
+        }
+    }
+
+    public static void encerrarSessao() {
+        usuario = null;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return login + " " + senha;
+    }
 }
