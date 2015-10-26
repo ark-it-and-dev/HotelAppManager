d696f9721f3b43be8143ccbda747cf41e40f69bb
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-10-26 00:47:40 -0200

    * Conclusao da classe UsuarioService

--- HotelAppManager/app/src/main/java/br/metodista/hotelappmanager/webservice/UsuarioService.java
+++ HotelAppManager/app/src/main/java/br/metodista/hotelappmanager/webservice/UsuarioService.java
@@ -1,7 +1,103 @@
 package br.metodista.hotelappmanager.webservice;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import br.metodista.hotelappmanager.model.Usuario;
+
 /**
  * Created by Gustavo Assalin on 26/10/2015.
  */
 public class UsuarioService {
+    private static final String URL = "https://openws.herokuapp.com/hotelappusuarios";
+    private static final String API_KEY = "b4111c92d8a0f3d61f3cfd87e9a4eb75";
+
+    private HttpURLConnection urlConnection = null;
+
+    public List<Usuario> getAll() {
+        List<Usuario> usuarios = new ArrayList<>();
+        HttpURLConnection urlConnection = null;
+
+        try {
+            java.net.URL url = new URL(URL + API_KEY);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in =  new BufferedInputStream(urlConnection.getInputStream());
+            Scanner s = new Scanner(in);
+            String conteudo = s.useDelimiter("\\A").next();
+
+            Gson gson = new Gson();
+            usuarios = gson.fromJson(conteudo, new TypeToken<List<Usuario>>(){}.getType());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            urlConnection.disconnect();
+        }
+
+        return usuarios;
+    }
+
+    public boolean save(Object usuario) {
+        boolean retorno = false;
+
+        try {
+            URL url = new URL(URL + API_KEY);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setRequestProperty("Accept", "application/json");
+
+            OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+            Writer w = new BufferedWriter(new OutputStreamWriter(out));
+            Gson gson = new Gson();
+            String json = gson.toJson(usuario);
+            w.write(json);
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+            Scanner s = new Scanner(in);
+            String conteudo = s.nextLine();
+
+            w.close();
+            in.close();
+            retorno = true;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            urlConnection.disconnect();
+        }
+
+        return retorno;
+    }
+
+    public void delete(String id) {
+        HttpURLConnection urlConnection = null;
+
+        try {
+            URL url = new URL(URL + "/" + id + API_KEY);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
