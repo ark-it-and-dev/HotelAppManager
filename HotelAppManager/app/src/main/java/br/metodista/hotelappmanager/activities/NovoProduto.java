d994016398db722ab5c2e6e8d2404143b66a1133
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-10-26 00:07:41 -0200

    + Criacao default da Empty activity NovoProduto e alteracao altomatica no Manifest

---
+++ HotelAppManager/app/src/main/java/br/metodista/hotelappmanager/activities/NovoProduto.java
@@ -0,0 +1,15 @@
+package br.metodista.hotelappmanager.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.metodista.hotelappmanager.R;
+
+public class NovoProduto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_produto);
+    }
+}
