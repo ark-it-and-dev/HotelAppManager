9458c88b626e0bf6bf192eedf865690870380d02
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-11-23 21:14:44 -0200

    + Adicao da Activity NovoUsuario

--- HotelAppManager/app/src/main/java/br/metodista/hotelappmanager/activities/NovoUsuario.java
+++ HotelAppManager/app/src/main/java/br/metodista/hotelappmanager/activities/NovoUsuario.java
@@ -10,16 +10,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import br.metodista.hotelappmanager.R;
+import br.metodista.hotelappmanager.enumeration.CategoriaProduto;
+import br.metodista.hotelappmanager.model.Produto;
 import br.metodista.hotelappmanager.model.Usuario;
 import br.metodista.hotelappmanager.webservice.UsuarioService;
 
 public class NovoUsuario extends AppCompatActivity {
     private UsuarioService service = new UsuarioService();
 
-    private Usuario usuario = Usuario.getInstance();
+    private Usuario usuario = new Usuario();
 
     private EditText txtUsuario;
     private TextView txtSenha;
@@ -33,19 +37,40 @@ public class NovoUsuario extends AppCompatActivity {
         txtUsuario = (EditText) findViewById(R.id.txtNome);
         txtSenha = (TextView) findViewById(R.id.txtSenha);
 
-        String login = txtUsuario.getText().toString();
-        String senha = txtSenha.getText().toString();
-
-        usuario.setLogin(login);
-        usuario.setSenha(senha);
+        txtSenha.setText(sortearSenha());
 
         btnSalvar = (Button) findViewById(R.id.btnSalvar);
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                usuario.setLogin(txtUsuario.getText().toString());
+                usuario.setSenha(txtSenha.getText().toString());
+
+                List<Produto> lista = new ArrayList<Produto>();
+
+                usuario.setListaProduto(lista);
+
                 new SalvarUsuario().execute();
+
+                txtUsuario.setText("");
+                txtSenha.setText(sortearSenha());
             }
         });
+    }
+
+    private String sortearSenha() {
+        char[] caracteresSenha =
+                {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'w', 'x', 'y', 'z',
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+
+        Random r = new Random();
+        String sorteio = "";
+
+        for(int contador = 0; contador <= 7; contador++) {
+            sorteio += caracteresSenha[r.nextInt(34)];
+        }
+
+        return sorteio;
     }
 
     private class SalvarUsuario extends AsyncTask<String, Void, List<Usuario>> {
@@ -56,16 +81,13 @@ public class NovoUsuario extends AppCompatActivity {
             super.onPreExecute();
 
             dialog = new ProgressDialog(NovoUsuario.this);
+            dialog.setMessage("Salvando");
             dialog.show();
         }
 
         @Override
         protected List<Usuario> doInBackground(String... params) {
-            if(service.save(usuario)) {
-                Toast.makeText(NovoUsuario.this, "Usuario Salvo com Sucesso!", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(NovoUsuario.this, "Falha ao Salvar o Usuário", Toast.LENGTH_SHORT).show();
-            }
+            service.save(usuario);
 
             return null;
         }
@@ -75,6 +97,8 @@ public class NovoUsuario extends AppCompatActivity {
             super.onPostExecute(usuarios);
 
             dialog.dismiss();
+
+            Toast.makeText(NovoUsuario.this, "Usuario Salvo com Sucesso!", Toast.LENGTH_SHORT).show();
         }
     }
 }
